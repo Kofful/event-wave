9d508a8d0b7b05c1a1475eea44e56a7ed7bfde0e
Vladyslav Cherniuk <vlad26v03@gmail.com>
2024-05-09 16:48:43 +0300

    Return 403 when user is not logged in

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -5,15 +5,21 @@ namespace App\Http\Middleware;
 use App\Models\User;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Validation\UnauthorizedException;
 
 class Authenticate extends Middleware
 {
     protected function authenticate($request, array $guards)
     {
-        parent::authenticate($request, $guards);
-
         /** @var User $authorizedUser */
         $authorizedUser = auth()->user();
+
+        if (!$authorizedUser) {
+            throw new UnauthorizedException();
+        }
+
+        parent::authenticate($request, $guards);
+
 
         if (!$authorizedUser->is_approved) {
             $this->unauthenticated($request, $guards);
